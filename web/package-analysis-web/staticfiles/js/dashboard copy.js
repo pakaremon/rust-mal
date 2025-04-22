1ae631c8bc05ccc3c2f1556b139cdc17413f1626
pakaremon <nguyenthanhcong012002@gmail.com>
2025-04-22 23:23:53 +0700

    Update web

--- web/package-analysis-web/staticfiles/js/dashboard copy.js
+++ web/package-analysis-web/staticfiles/js/dashboard copy.js
@@ -122,130 +122,34 @@ document.addEventListener("DOMContentLoaded", () => {
 */
 
 
-// document.addEventListener("DOMContentLoaded", () => {
-//     const input = document.getElementById("package_name");
-//     const packageVersion = document.getElementById("package_version");
-//     const ecosystem = document.getElementById("ecosystem");
-//     const suggestions = document.getElementById("suggestions");
-
-//     const loadPackages = () => {
-//         load().then(packages => {
-//             input.addEventListener("input", function() {
-//                 const value = this.value.toLowerCase();
-//                 suggestions.innerHTML = "";
-
-//                 let package_names = [];
-//                 if (ecosystem.value === "crates.io") {
-//                     package_names = Object.keys(packages);
-//                 } else if (ecosystem.value === "pypi") {
-//                     package_names = packages.packages;
-//                 } else if (ecosystem.value === "npm") {
-//                     package_names = packages.packages;
-//                 } else if (ecosystem.value === "packagist") {
-//                     package_names = packages.packages;
-//                 } else if (ecosystem.value === "rubygems") {
-//                     package_names = packages.packages;
-//                 }
-
-//                 if (value) {
-//                     const filteredPackages = package_names
-//                         .filter(package_name => package_name.toLowerCase().startsWith(value))
-//                         .sort((a, b) => a.localeCompare(b))
-//                         .slice(0, 1000);
-
-//                     if (filteredPackages.length > 0) {
-//                         suggestions.style.display = "block";
-//                         filteredPackages.forEach(package_name => {
-//                             const div = document.createElement("div");
-//                             div.textContent = package_name;
-//                             div.addEventListener("click", async function() {
-//                                 input.value = package_name;
-//                                 suggestions.style.display = "none";
-
-//                                 let versions = [];
-//                                 if (ecosystem.value === "crates.io") {
-//                                     versions = packages[package_name].slice().reverse();
-//                                 } else if (ecosystem.value === "pypi") {
-//                                     const pypiData = await get_pypi_versions(package_name);
-//                                     versions = pypiData.versions.slice().reverse();
-//                                 } else if (ecosystem.value === "npm") {
-//                                     const npmData = await get_npm_versions(package_name);
-//                                     versions = npmData.versions.slice().reverse();
-//                                 } else if (ecosystem.value === "packagist") {
-//                                     const packagistData = await get_packagist_versions(package_name);
-//                                     versions = packagistData.versions.slice();
-//                                 } else if (ecosystem.value === "rubygems") {
-//                                     const rubygemsData = await get_rubygems_versions(package_name);
-//                                     versions = rubygemsData.versions.slice();
-//                                 }
-
-
-//                                 packageVersion.innerHTML = ""; // Clear previous options
-//                                 packageVersion.style.display = "block"; // Show the select box
-//                                 packageVersion.removeAttribute("disabled"); // Enable the select box
-//                                 if (versions.length > 0) {
-//                                     versions.forEach(version => {
-//                                         const option = document.createElement("option");
-//                                         option.value = version;
-//                                         option.textContent = version;
-//                                         packageVersion.appendChild(option);
-//                                     });
-//                                 } else {
-//                                     packageVersion.style.display = "none";
-//                                 }
-//                             });
-//                             suggestions.appendChild(div);
-//                         });
-//                     } else {
-//                         suggestions.style.display = "none";
-//                     }
-//                 } else {
-//                     suggestions.style.display = "none";
-//                 }
-//             });
-//         });
-//     };
-
-//     // Load packages initially
-//     loadPackages();
-
-//     // Reload packages when ecosystem changes
-//     ecosystem.addEventListener("change", () => {
-//         input.value = ""; // Clear the input field
-//         suggestions.innerHTML = ""; // Clear suggestions
-//         packageVersion.innerHTML = ""; // Clear package versions
-//         packageVersion.style.display = "none"; // Hide the select box
-//         loadPackages(); // Reload packages
-//     });
-
-//     document.addEventListener("click", (e) => {
-//         if (!document.querySelector("#package_name").contains(e.target) && !suggestions.contains(e.target)) {
-//             suggestions.style.display = "none";
-//         }
-//     });
-// });
-
-
 document.addEventListener("DOMContentLoaded", () => {
     const input = document.getElementById("package_name");
+    const packageVersion = document.getElementById("package_version");
     const ecosystem = document.getElementById("ecosystem");
     const suggestions = document.getElementById("suggestions");
-    const versionSuggestions = document.getElementById("version-suggestions");
-    const packageVersion = document.getElementById("package_version");
 
     const loadPackages = () => {
         load().then(packages => {
-            input.addEventListener("input", function () {
+            input.addEventListener("input", function() {
                 const value = this.value.toLowerCase();
                 suggestions.innerHTML = "";
-                versionSuggestions.innerHTML = ""; // Clear previous version suggestions
 
                 let package_names = [];
                 if (ecosystem.value === "crates.io") {
                     package_names = Object.keys(packages);
-                } else if (["pypi", "npm", "packagist", "rubygems"].includes(ecosystem.value)) {
+                } else if (ecosystem.value === "pypi") {
                     package_names = packages.packages;
-            }  
+                } else if (ecosystem.value === "npm") {
+                    package_names = packages.packages;
+                } else if (ecosystem.value === "packagist") {
+                    package_names = packages.packages;
+                } else if (ecosystem.value === "rubygems") {
+                    package_names = packages.packages;
+                } else if (ecosystem.value === "maven") { 
+                    // change place holder of maven packages name is group_id:artifact_id
+                    input.placeholder = "group_id:artifact_id such as com.google.guava:guava";
+
+                }
 
                 if (value) {
                     const filteredPackages = package_names
@@ -258,7 +162,7 @@ document.addEventListener("DOMContentLoaded", () => {
                         filteredPackages.forEach(package_name => {
                             const div = document.createElement("div");
                             div.textContent = package_name;
-                            div.addEventListener("click", async function () {
+                            div.addEventListener("click", async function() {
                                 input.value = package_name;
                                 suggestions.style.display = "none";
 
@@ -278,23 +182,20 @@ document.addEventListener("DOMContentLoaded", () => {
                                     const rubygemsData = await get_rubygems_versions(package_name);
                                     versions = rubygemsData.versions.slice();
                                 }
-                                versionSuggestions.innerHTML = ""; // Clear previous suggestions
+
+
+                                packageVersion.innerHTML = ""; // Clear previous options
+                                packageVersion.style.display = "block"; // Show the select box
+                                packageVersion.removeAttribute("disabled"); // Enable the select box
                                 if (versions.length > 0) {
-                                    versionSuggestions.style.display = "block"; // Show version suggestions
                                     versions.forEach(version => {
-                                        const versionDiv = document.createElement("div");
-                                        versionDiv.textContent = version;
-                                        versionDiv.addEventListener("click", function () {
-                                            packageVersion.value = version; // Set the selected version in the input form
-                                            versionSuggestions.style.display = "none"; // Hide version suggestions
-                                        });
-                                        versionSuggestions.appendChild(versionDiv);
+                                        const option = document.createElement("option");
+                                        option.value = version;
+                                        option.textContent = version;
+                                        packageVersion.appendChild(option);
                                     });
                                 } else {
-                                    const noVersionsDiv = document.createElement("div");
-                                    noVersionsDiv.textContent = "No versions available";
-                                    noVersionsDiv.classList.add("no-versions");
-                                    versionSuggestions.appendChild(noVersionsDiv);
+                                    packageVersion.style.display = "none";
                                 }
                             });
                             suggestions.appendChild(div);
@@ -316,8 +217,8 @@ document.addEventListener("DOMContentLoaded", () => {
     ecosystem.addEventListener("change", () => {
         input.value = ""; // Clear the input field
         suggestions.innerHTML = ""; // Clear suggestions
-        versionSuggestions.innerHTML = ""; // Clear version suggestions
-        versionSuggestions.value = ""; // Clear selected version
+        packageVersion.value = ""; // Clear package versions
+        packageVersion.style.display = "none"; // Hide the select box
         loadPackages(); // Reload packages
     });
 
